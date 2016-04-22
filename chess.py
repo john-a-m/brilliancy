99d0a5e8e84d031cfe8c8f6cef6493b5c5622f67
John <johnthexiii@gmail.com>
2016-04-21 22:21:20 -0400

    pulled move generation into it's own function

--- chess.py
+++ chess.py
@@ -1,11 +1,5 @@
 #TODO implement captures as possible moves
 
-#TODO reduce
-#   new_grid = [[s for s in row] for row in grid]
-#   new_grid[self.y][self.x] = None
-#   new_grid[y][x] = self
-#into a function or something
-
 class Piece(object):
     def __init__(self, color, x, y):
         self.color = color
@@ -44,10 +38,7 @@ class Knight(Piece):
         for move in moves:
             x, y = move
             if x >= 0 and x < len(grid) and y >= 0 and y < len(grid):
-                new_grid = [[s for s in row] for row in grid]
-                new_grid[self.y][self.x] = None
-                new_grid[y][x] = self
-                yield new_grid
+                yield _move_piece(self, grid, x, y)
 
 class Bishop(Piece):
 
@@ -79,27 +70,22 @@ class King(Piece):
 
     def get_moves(self, grid):
 
-        #TODO reverse x and y
         moves = [
-            (self.y - 1, self.x),
-            (self.y - 1, self.x + 1),
-            (self.y, self.x + 1),
-            (self.y + 1, self.x + 1),
-            (self.y + 1, self.x),
-            (self.y + 1, self.x - 1),
-            (self.y, self.x - 1),
-            (self.y - 1, self.x - 1)
+            (self.x, self.y - 1),
+            (self.x + 1, self.y - 1),
+            (self.x + 1, self.y),
+            (self.x + 1, self.y + 1),
+            (self.x, self.y + 1),
+            (self.x - 1, self.y + 1),
+            (self.x - 1, self.y),
+            (self.x - 1, self.y - 1)
         ]
 
         for move in moves:
             x, y = move
             if x >= 0 and x < len(grid) and y >= 0 and y < len(grid):
-                new_grid = [[s for s in row] for row in grid]
-                new_grid[self.y][self.x] = None
-                new_grid[y][x] = self
-                yield new_grid
+                yield _move_piece(self, grid, x, y)
             
-
 class Pawn(Piece):
 
     value = 1
@@ -114,45 +100,30 @@ class Pawn(Piece):
         for move in moves:
             x, y = move
             if x >= 0 and x < len(grid) and y >= 0 and y < len(grid):
-                new_grid = [[s for s in row] for row in grid]
-                new_grid[self.y][self.x] = None
-                new_grid[y][x] = self
-                yield new_grid
+                yield _move_piece(self, grid, x, y)
  
 def _get_horizontal_moves(grid, piece):
 
     index = piece.x - 1
     while index >= 0 and grid[piece.y][index] is None:
-        new_grid = [[s for s in row] for row in grid]
-        new_grid[piece.y][piece.x] = None
-        new_grid[piece.y][index] = piece
-        yield new_grid
+        yield _move_piece(piece, grid, x, y)
         index -= 1
 
     index = piece.x + 1
     while index < len(grid) and grid[piece.y][index] is None:
-        new_grid = [[s for s in row] for row in grid]
-        new_grid[piece.y][piece.x] = None
-        new_grid[piece.y][index] = piece
-        yield new_grid
+        yield _move_piece(piece, grid, x, y)
         index += 1
         
 def _get_vertical_moves(grid, piece):
     
     index = piece.y - 1
     while index >= 0 and grid[index][piece.x] is None:
-        new_grid = [[s for s in row] for row in grid]
-        new_grid[piece.y][piece.x] = None
-        new_grid[index][piece.x] = piece
-        yield new_grid
+        yield _move_piece(piece, grid, x, y)
         index -= 1
 
     index = piece.x + 1
     while index < len(grid) and grid[index][piece.x] is None:
-        new_grid = [[s for s in row] for row in grid]
-        new_grid[piece.y][piece.x] = None
-        new_grid[index][piece.x] = piece
-        yield new_grid
+        yield _move_piece(piece, grid, x, y)
         index += 1
 
 def _get_diagonal_moves(grid, piece):
@@ -161,55 +132,54 @@ def _get_diagonal_moves(grid, piece):
     #up left
     x, y = piece.x - 1, piece.y - 1
     while x >= 0 and y >= 0 and grid[y][x] is None:
-        new_grid = [[s for s in row] for row in grid]
-        new_grid[piece.y][piece.x] = None
-        new_grid[y][x] = piece
-        yield new_grid
+        yield _move_piece(piece, grid, x, y)
         x -= 1
         y -= 1
 
     #down right
     x, y = piece.x + 1, piece.y + 1
     while x >= 0 and y >= 0 and grid[y][x] is None:
-        new_grid = [[s for s in row] for row in grid]
-        new_grid[piece.y][piece.x] = None
-        new_grid[y][x] = piece
-        yield new_grid
+        yield _move_piece(piece, grid, x, y)
         x += 1
         y += 1
 
     #up right
     x, y = piece.x + 1, piece.y - 1
     while x >= 0 and y >= 0 and grid[y][x] is None:
-        new_grid = [[s for s in row] for row in grid]
-        new_grid[piece.y][piece.x] = None
-        new_grid[y][x] = piece
-        yield new_grid
+        yield _move_piece(piece, grid, x, y)
         x += 1
         y -= 1
 
     #down left
     x, y = piece.x - 1, piece.y + 1
-    while x >= 0 and y >= 0 and grid[y][x] is None:
-        new_grid = [[s for s in row] for row in grid]
-        new_grid[piece.y][piece.x] = None
-        new_grid[y][x] = piece
-        yield new_grid
+    while x >= 0 and y >= 0:
+        yield _move_piece(piece, grid, x, y)
+
+        if grid[y][x] is not None and grid[y][x].color != piece.color:
+            break
+        
         x -= 1
         y += 1
+
+def _move_piece(piece, grid, x, y):
+    #TODO put bounds checking in here
+    new = [[s for s in row] for row in grid]
+    new[piece.y][piece.x] = None
+    new[y][x] = piece
+    return new
 
 BLACK = 0
 WHITE = 1
 
-grid = [
-    [Rook(WHITE), Knight(WHITE), Bishop(WHITE), King(WHITE), Queen(WHITE),
-     Bishop(WHITE), Knight(WHITE), Rook(WHITE)],
-    [Pawn(WHITE) for _ in range(8)],
-    [None] * 8,
-    [None] * 8,
-    [None] * 8,
-    [None] * 8,
-    [Pawn(BLACK) for _ in range(8)],
-    [Rook(BLACK), Knight(BLACK), Bishop(BLACK), Queen(BLACK), King(BLACK),
-     Bishop(BLACK), Knight(BLACK), Rook(BLACK)]
-    ]
+##grid = [
+##    [Rook(WHITE), Knight(WHITE), Bishop(WHITE), King(WHITE), Queen(WHITE),
+##     Bishop(WHITE), Knight(WHITE), Rook(WHITE)],
+##    [Pawn(WHITE) for _ in range(8)],
+##    [None] * 8,
+##    [None] * 8,
+##    [None] * 8,
+##    [None] * 8,
+##    [Pawn(BLACK) for _ in range(8)],
+##    [Rook(BLACK), Knight(BLACK), Bishop(BLACK), Queen(BLACK), King(BLACK),
+##     Bishop(BLACK), Knight(BLACK), Rook(BLACK)]
+##    ]
